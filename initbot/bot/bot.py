9a3280eac2729f29e9910b96971eb42045996db6
Ihor Kuz <ihor@kry10.com>
2021-11-08 12:01:35 +1100

    WIP sound

--- initbot/bot/bot.py
+++ initbot/bot/bot.py
@@ -1,17 +1,51 @@
 from discord.ext.commands import Bot  # type: ignore
 
+from discord import opus
+
 from .config import CFG
 from .commands import commands
 
 bot = Bot(command_prefix="$")
 
+OPUS_LIBS = [
+    "libopus-0.x86.dll",
+    "libopus-0.x64.dll",
+    "libopus-0.dll",
+    "libopus.so.0",
+    "libopus.0.dylib",
+]
+
+
+def load_opus_lib():
+    if opus.is_loaded():
+        print("opus is loaded")
+        return True
+
+    print("opus not yet loaded")
+
+    for opus_lib in OPUS_LIBS:
+        try:
+            opus.load_opus(opus_lib)
+            return True
+        except OSError:
+            pass
+
+        raise RuntimeError(
+            "Could not load an opus lib."  # " Tried %s" % (", ".join(OPUS_LIBS))
+        )
+
 
 @bot.event
 async def on_ready():
-    print("Logged in as {0.user}".format(bot))
+    print("Logged in")  # as {0.user}".format(bot))
 
 
 def run():
+    load_opus_lib()
+    if not opus.is_loaded():
+        print("Opus is not loaded")
+    else:
+        print("Opus is loaded")
     for cmd in commands:
         bot.add_command(cmd)
     bot.run(CFG.token)
--- initbot/bot/commands/__init__.py
+++ initbot/bot/commands/__init__.py
@@ -1,32 +1,34 @@
 from typing import FrozenSet, Any
 
-from .crit import crit
-from .init import init, inis
-from .roll import roll
-from .character import new, set_, remove, chars, char
-from .occupation import occupations
-from .ability import abls, abl, asms, asm
-from .augur import augurs, augur
+# from .crit import crit
+# from .init import init, inis
+# from .roll import roll
+# from .character import new, set_, remove, chars, char
+# from .occupation import occupations
+# from .ability import abls, abl, asms, asm
+# from .augur import augurs, augur
 from .tarot import tarot
+from .sound import sound
 
 commands: FrozenSet[Any] = frozenset(
     (
-        crit,
-        init,
-        inis,
-        roll,
-        new,
-        char,
-        chars,
-        set_,
-        remove,
-        occupations,
-        abls,
-        abl,
-        asms,
-        asm,
-        augurs,
-        augur,
+        # crit,
+        # init,
+        # inis,
+        # roll,
+        # new,
+        # char,
+        # chars,
+        # set_,
+        # remove,
+        # occupations,
+        # abls,
+        # abl,
+        # asms,
+        # asm,
+        # augurs,
+        # augur,
         tarot,
+        sound,
     )
 )
